6ceb7af226dd9ab59b3c76cc9f5a1a7157e63009
Jone <jse1007@alu.ubu.es>
2024-12-17 11:28:00 +0100

    ModularidadTests

---
+++ PacManTests/Assets/Tests/EditMode_Tests/PlayerMovementTest_EM.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerMovementTest_EM
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void PlayerMovementTest_EMSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
